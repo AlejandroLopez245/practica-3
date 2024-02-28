dcc746b4ddd1c5f1fdef2851027c22f38318876a
AlejandroLopez245 <lopezramirezalejandrojesus1cpv@cetis50cdmx.com>
2024-02-28 13:15:18 -0600

    se suben los archivos a git

---
+++ operadores.js
@@ -0,0 +1,37 @@
+function suma(){
+    let num1 = Number(document.getElementById('n1').value);
+    let num2 = Number(document.getElementById('n2').value);
+
+    let res = num1 + num2;
+    
+    document.getElementById('res').value = res;
+}
+function resta(){
+    let num1 = Number(document.getElementById('n1').value);
+    let num2 = Number(document.getElementById('n2').value);
+    
+    let res = num1 = num2;
+    document.getElementById('res').value = res;
+
+}
+function multiplicacion() {
+    let num1 = Number(document.getElementById('n1').value);
+    let num2 = Number(document.getElementById('n2').value);
+
+    let res = num1*num2;
+
+    document.getElementById('res').value = res;
+}
+
+function division(){
+    let num1 = Number(document.getElementById('n1').value);
+    let num2 = Number(document.getElementById('n2').value);
+
+    //Manejo de la divicon pior cero
+    if (num2 !==0){
+        let res = num1/num2;
+        document.getElementById('res').value = res;
+    } else{
+        alert("No se puede dividir por cero :).");
+    }
+}
